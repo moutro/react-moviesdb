f9915be2512858f695f775b4b05d00ef02a97a92
Jason B <philotimo@gmail.com>
2019-12-24 12:28:58 -0500

    Updates

---
+++ precache-manifest.0a64bff08bf9e5a93fd79f7010fd7895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88c3fc6555f0bc615ff0f295620ef33",
+    "url": "/react-moviesdb/index.html"
+  },
+  {
+    "revision": "c91e27d0d35065e1d6d0",
+    "url": "/react-moviesdb/static/css/main.90d316f0.chunk.css"
+  },
+  {
+    "revision": "3ef2706b50379abe3fd2",
+    "url": "/react-moviesdb/static/js/2.0e4c1dfa.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/react-moviesdb/static/js/2.0e4c1dfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c91e27d0d35065e1d6d0",
+    "url": "/react-moviesdb/static/js/main.73f31609.chunk.js"
+  },
+  {
+    "revision": "db73aa4aa6ecc6a8d36c",
+    "url": "/react-moviesdb/static/js/runtime-main.2b5b6e60.js"
+  }
+]);
